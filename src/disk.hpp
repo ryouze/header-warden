6a77e8e2473e139e8a7cc3062d09a197edcd176e
ryouze <98982999+ryouze@users.noreply.github.com>
2024-03-28 00:34:31 +0100

    [src] Add preliminary disk header.

--- src/disk.hpp
+++ src/disk.hpp
@@ -1,173 +1,281 @@
+/**
+ * @file disk.hpp
+ *
+ * @brief Disk I/O functions.
+ */
+
 #pragma once
 
-#include <cstddef>  // for std::size_t
-#include <regex>    // for std::regex
-#include <string>   // for std::string
-#include <vector>   // for std::vector
+#include "core/log.hpp"
+#include "strings.hpp"
+
+#include <algorithm>
+#include <cstddef>     // for std::size_t
+#include <filesystem>  // for std::filesystem
+#include <fstream>     // for std::ifstream
+#include <iterator>    // for std::istreambuf_iterator
+#include <regex>
+#include <sstream>    // for std::istringstream
+#include <stdexcept>  // for std::runtime_error
+#include <string>     // for std::string, std::getline, std::to_string
+#include <vector>     // for std::vector
 
 namespace disk {
 
 /**
- * @brief Enum class representing the type of a line in a file.
+ * @brief Struct that represents a single line in a file.
  */
-enum class LineType {
-    BARE_INCLUDE,           // Represents a line with an include directive (e.g., "#include <string>")
-    INCLUDE_WITH_FUNCTION,  // Represents a line with an include directive and standard library functions (e.g., "#include <iostream> // for std::cout, std::cerr")
-    FUNCTION,               // Represents a line with a standard library function (e.g., "std::string")
-    EMPTY,                  // Represents an empty line
-};
+struct Line {
 
-/**
- * @brief Class that represents a single line of source code.
- *
- * On construction, the line's content is parsed and its type is determined. The include directive and standard library functions are extracted from the line's text and stored as the object's members.
- */
-class Line {
-  public:
     /**
      * @brief Construct a new Line object.
      *
-     * @param line_number Line number (e.g., 20).
-     * @param line_text Line text (e.g., "hello").
-     *
-     * @throws std::runtime_error if failed to process the line.
+     * @param number Line number (e.g., "5").
+     * @param text Line text (e.g., "std::string foo()").
      */
-    explicit Line(const std::size_t line_number,
-                  const std::string &line_text);
+    explicit Line(const size_t number, const std::string &text)
+        : number(number), text(text) {}
 
     /**
-     * @brief Get the line number.
-     *
-     * @return Line number (e.g., "20").
+     * @brief Line number (e.g., "5").
      */
-    [[nodiscard]] std::size_t get_number() const;
+    const size_t number;
 
     /**
-     * @brief Get the line text.
-     *
-     * @return Line text (e.g., "hello").
+     * @brief Line text (e.g., "std::string foo()").
      */
-    [[nodiscard]] const std::string &get_text() const;
-
-    /**
-     * @brief Get the line type.
-     *
-     * This is based on the line's content, and can be one of the following:
-     * - "BARE_INCLUDE": Represents a line with a bare include directive (e.g., "#include <string>").
-     * - "INCLUDE_WITH_FUNCTION": Represents a line with an include directive and standard library functions (e.g., "#include <iostream> // for std::cout, std::cerr").
-     * - "FUNCTION": Represents a line with a standard library function (e.g., "std::string").
-     * - "EMPTY": Represents an empty line.
-     *
-     * @return Line type (e.g., "BARE_INCLUDE").
-     */
-    [[nodiscard]] LineType get_type() const;
-
-    /**
-     * @brief Get the standard library functions without the "std::" prefix.
-     *
-     * @return Standard library functions (e.g., {"cout", "cerr"}). If no functions are present, an empty vector is returned.
-     */
-    [[nodiscard]] const std::vector<std::string> &get_functions() const;
-
-    /**
-     * @brief Get the standard library without the "#include <>" directive.
-     *
-     * @return Library name (e.g., "iostream"). If no library is included in the line, an empty string is returned.
-     */
-    [[nodiscard]] const std::string &get_include() const;
-
-  private:
-    /**
-     * @brief Check if the line contains a given regular expression pattern.
-     *
-     * @param regex_pattern Regular expression pattern to search for (e.g., "he.*o").
-     *
-     * @return True if the line contains the regex pattern, false otherwise (e.g., "true").
-     */
-    [[nodiscard]] bool contains_regex(const std::regex &regex_pattern) const;
-
-    /**
-     * @brief Get the first match of a regular expression pattern in the line.
-     *
-     * @param regex_pattern Regular expression pattern to search for (e.g., "he(.*)o").
-     * @param capture_group Capture group to be used. If set to "0", the entire matched string is returned. If set to "1", the first capture group is returned. Defaults to "0".
-     *
-     * @return First match for the given regex pattern and capture group (e.g., "ll"). If no match is found, an empty string is returned.
-     */
-    [[nodiscard]] std::string get_first_regex_match(const std::regex &regex_pattern,
-                                                    const std::size_t capture_group = 0) const;
-
-    /**
-     * @brief Get all matches of a regular expression pattern in the line.
-     *
-     * @param regex_pattern Regular expression pattern to search for (e.g., "he(.*)o").
-     * @param capture_group Capture group to be used. If set to "0", the entire matched string is returned. If set to "1", the first capture group is returned. Defaults to "0".
-     * @param remove_comments Whether to remove comments before checking for matches. The original string is not modified. If set to "true", comments are removed before checking for matches. If set to "false", comments are left in the source code. Defaults to "true".
-     *
-     * @return All matches for the given regex pattern and capture group (e.g., {"ll", "a"}). If no matches are found, an empty vector is returned.
-     */
-    [[nodiscard]] std::vector<std::string> get_all_regex_matches(const std::regex &regex_pattern,
-                                                                 const std::size_t capture_group = 0,
-                                                                 const bool remove_comments = true) const;
-
-    /**
-     * @brief Line number (e.g., "20").
-     */
-    const std::size_t number_;
-
-    /**
-     * @brief Line text (e.g., "hello").
-     */
-    const std::string text_;
-
-    /**
-     * @brief Line type (e.g., "BARE_INCLUDE").
-     */
-    LineType type_;
-
-    /**
-     * @brief Standard library functions (e.g., {"cout", "cerr"}).
-     */
-    std::vector<std::string> functions_;
-
-    /**
-     * @brief Library name (e.g., "iostream").
-     */
-    std::string include_;
+    const std::string text;
 };
 
 /**
- * @brief Class that represents a source code file.
- *
- * On construction, the file's content is read and stored as the object member.
+ * @brief Struct that represents a bare include directive in a file.
+ */
+struct BareInclude {
+
+    /**
+     * @brief Construct a new BareInclude object.
+     *
+     * @param line Line where the include directive is found.
+     * @param name Name of the included file (e.g., "#include <iostream>").
+     */
+    BareInclude(const Line &line, const std::string &name)
+        : line(line), name(name) {}
+
+    /**
+     * @brief Line where the include directive is found.
+     */
+    const Line line;
+
+    /**
+     * @brief Name of the included file (e.g., "#include <iostream>").
+     */
+    const std::string name;
+};
+
+/**
+ * @brief Struct that represents an include directive with associated functions in a file.
+ */
+struct IncludeWithFunctions {
+
+    /**
+     * @brief Construct a new IncludeWithFunctions object.
+     *
+     * @param line Line where the include directive is found.
+     * @param name Name of the included file (e.g., "#include <iostream>").
+     * @param functions Functions associated with the include directive (e.g., "std::cout").
+     */
+    IncludeWithFunctions(const Line &line, const std::string &name, const std::vector<std::string> &functions)
+        : line(line), name(name), functions(functions) {}
+
+    /**
+     * @brief Line where the include directive is found.
+     */
+    const Line line;
+
+    /**
+     * @brief Name of the included file (e.g., "#include <iostream>").
+     */
+    const std::string name;
+
+    /**
+     * @brief Functions associated with the include directive (e.g., "std::cout").
+     */
+    const std::vector<std::string> functions;
+};
+
+/**
+ * @brief Struct that represents a free-standing function not in an include directive in a file.
+ */
+struct Functions {
+
+    /**
+     * @brief Construct a new Functions object.
+     *
+     * @param line Line where the function is found.
+     * @param functions Functions found on the line (e.g., "std::cout").
+     */
+    Functions(const Line &line, const std::vector<std::string> &functions)
+        : line(line), functions(functions) {}
+
+    /**
+     * @brief Line where the function is found.
+     */
+    const Line line;
+
+    /**
+     * @brief Functions found on the line.
+     */
+    const std::vector<std::string> functions;
+};
+
+/**
+ * @brief Class that extracts information from a file.
  */
 class File {
   public:
-    /**
-     * @brief Construct a new File object.
-     *
-     * @param file_path Path to the source code file (e.g., "~/data/file.txt").
-     * @param turn_lowercase Whether to turn the source code lowercase. If set to "true", the source code will be turned lowercase. If set to "false", the source code will be left in its original case, which might cause problems with regular expressions. Defaults to "true".
-     *
-     * @throws std::runtime_error if failed to open the source code file.
-     */
-    explicit File(const std::string &file_path,
-                  const bool turn_lowercase = true);
+    explicit File(const std::string &file_path)
+    {
+        // Load the file from disk and iterate over each line
+        for (const Line &line : this->load_lines(file_path)) {
 
-    /**
-     * @brief Get all lines of the source code file.
-     *
-     * The Line object automatically parses the line's content and type on construction. It contains line number, text, type, and other members.
-     *
-     * @return All lines as Line objects.
-     */
-    [[nodiscard]] const std::vector<Line> &get_lines() const;
+            // The trick here is that we print the original line, but we actually operate on the modified line
+            LOG_DEBUG("Processing line no. " + std::to_string(line.number) + ": " + line.text);
+
+            // Strip leading and trailing whitespace from line, turn line lowercase
+            std::string modified_line = strings::to_lower(strings::trim_whitespace(line.text));
+            LOG_DEBUG("Line after removing leading & trailing whitespace and turning it to lowercase: " + modified_line);
+
+            // Skip if the modified line is a comment
+            if (modified_line.find("//") == 0 || modified_line.find("*") == 0) {
+                LOG_DEBUG("Skipping line, because the line is a comment");
+                continue;
+            }
+            // Skip if the modified line is empty
+            else if (modified_line.empty()) {
+                LOG_DEBUG("Skipping line, because the line is empty");
+                continue;
+            }
+            // Otherwise, process it with regex
+            else {
+                // Extracted the include directive regex as a constant to avoid recompilation
+                static const std::regex include_directive_regex(R"(#include\s*<\S+>)");
+
+                // Extracted the function call regex as a constant to avoid recompilation
+                static const std::regex function_call_regex(R"(std::(\w+))");
+
+                // 1) Check if the line contains an include directive
+                std::smatch include_match;
+                const bool line_contains_include = std::regex_search(modified_line, include_match, include_directive_regex);
+
+                if (line_contains_include) {
+                    const std::string include_directive = include_match.str(0);
+                    LOG_INFO("Found include directive '" + include_directive + "'");
+                }
+
+                // 2) Get all std::function calls in the modified line
+                std::vector<std::string> function_matches;
+
+                // If line_contains_include is false, we remove comments to prevent false positives
+                if (!line_contains_include) {
+                    modified_line = strings::remove_comments(modified_line);
+                }
+
+                // Create const iterators pointing to the start and end of the sequence of matches
+                std::sregex_iterator begin(modified_line.cbegin(), modified_line.cend(), function_call_regex), end;
+
+                // Use std::transform to iterate over each match in the sequence
+                std::transform(begin, end, std::back_inserter(function_matches),
+                               [](const std::smatch &match) {
+                                   return match.str(0);
+                               });
+
+                if (function_matches.empty()) {
+                    LOG_INFO("VERDICT: No std::function calls found in the line");
+                }
+                else {
+                    LOG_INFO("VERDICT: Found the following std::function calls: " + strings::vector_to_string(function_matches));
+                }
+
+                // Categorize the result into containers
+                if (line_contains_include && !function_matches.empty()) {  // #include <iostream> // for std::cout, std::cerr
+                    this->includes_with_functions.emplace_back(line, include_match.str(0), function_matches);
+                }
+                else if (line_contains_include) {  // #include <string>
+                    this->bare_includes.emplace_back(line, include_match.str(0));
+                }
+                else if (!function_matches.empty()) {  // std::string
+                    this->functions.emplace_back(line, function_matches);
+                }
+            }
+        }
+    }
+
+    const std::vector<disk::BareInclude> &get_bare_includes() const
+    {
+        return bare_includes;
+    }
+
+    const std::vector<disk::IncludeWithFunctions> &get_includes_with_functions() const
+    {
+        return includes_with_functions;
+    }
+
+    const std::vector<disk::Functions> &get_functions() const
+    {
+        return functions;
+    }
 
   private:
     /**
-     * @brief Lines of the source code file.
-     */
-    std::vector<Line> lines_;
+   * @brief Load lines from a file and return them as a vector of Line objects.
+   *
+   * Each Line object contains the line number and text.
+   *
+   * @param file_path Path to the file to load (e.g., "src/main.cpp").
+   *
+   * @return Vector of Line objects, with each Line representing a single line in the file (e.g., "{Line(1, "#include <iostream>"), Line(2, "int main() {")").
+
+   * @throws std::runtime_error If the file_path does not exist, is not a regular file, or if there was an error opening the file.
+   *
+   * @note The line numbers are 1-based.
+   */
+    std::vector<Line> load_lines(const std::string &file_path)
+    {
+        // Throw if the file does not exist
+        if (!std::filesystem::exists(file_path)) {
+            throw std::runtime_error("File does not exist: " + file_path);
+        }
+
+        // Throw if the path is not a file
+        if (!std::filesystem::is_regular_file(file_path)) {
+            throw std::runtime_error("Path is not a file: " + file_path);
+        }
+
+        // Load a file from disk
+        LOG_INFO("Loading file from disk: " + file_path);
+        std::ifstream file(file_path);
+        if (!file) {
+            throw std::runtime_error("Failed to open file: " + file_path);
+        }
+
+        // Read the entire file into a string at once
+        std::string content((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
+
+        // Split the content into lines, this is a bit more efficient than reading line by line
+        std::vector<Line> lines;
+        std::size_t line_number = 1;
+        std::istringstream stream(content);
+        for (std::string buffer; std::getline(stream, buffer); ++line_number) {
+            lines.emplace_back(line_number, buffer);
+        }
+
+        // Return the lines
+        return lines;
+    }
+
+    std::vector<disk::BareInclude> bare_includes;
+    std::vector<disk::IncludeWithFunctions> includes_with_functions;
+    std::vector<disk::Functions> functions;
 };
 
 }  // namespace disk
