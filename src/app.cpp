ec8fd711d4a37f7d3a27c26781be48107be17342
ryouze <98982999+ryouze@users.noreply.github.com>
2024-06-25 20:00:03 +0200

    [src] Simplify constructing report.

--- src/app.cpp
+++ src/app.cpp
@@ -24,25 +24,21 @@ namespace {
  */
 [[nodiscard]] std::string get_report(const std::string &file_path)
 {
-    static const std::string line_separator(80, '-');
-
-    bool has_issues = false;
-
     LOG_DEBUG("Processing file: " + file_path);
-    const io::disk::File processed_file(file_path);
 
+    static const std::string line_separator(80, '-');
+    bool has_issues = false;
     std::stringstream report_message;
-    report_message << "##- " << file_path << " -##\n\n";
+
+    // Load the file from disk and process it on construction
+    const io::disk::File processed_file(file_path);
 
     // Process bare includes if enabled
     if (core::globals::bare) {
-        report_message << "-- 1) BARE INCLUDES --\n\n";
         const auto &bare_includes = processed_file.get_bare_includes();
-        if (bare_includes.empty()) {
-            report_message << "-> No bare includes found.\n\n";
-        }
-        else {
+        if (!bare_includes.empty()) {
             has_issues = true;
+            report_message << "-- 1) BARE INCLUDES --\n\n";
             for (const auto &bare_include : bare_includes) {
                 report_message << bare_include.line.number << "| " << bare_include.line.text << '\n'
                                << "-> Bare include directive.\n"
@@ -53,13 +49,10 @@ namespace {
 
     // Process unused functions if enabled
     if (core::globals::unused) {
-        report_message << "-- 2) UNUSED FUNCTIONS --\n\n";
         const auto &unused_functions = processed_file.get_unused_functions();
-        if (unused_functions.empty()) {
-            report_message << "-> No unused functions found.\n\n";
-        }
-        else {
+        if (!unused_functions.empty()) {
             has_issues = true;
+            report_message << "-- 2) UNUSED FUNCTIONS --\n\n";
             for (const auto &unused_function : unused_functions) {
                 report_message << unused_function.line.number << "| " << unused_function.line.text << '\n'
                                << "-> Unused functions listed as comments.\n"
@@ -70,13 +63,10 @@ namespace {
 
     // Process missing functions if enabled
     if (core::globals::unlisted) {
-        report_message << "-- 3) UNLISTED FUNCTIONS --\n\n";
         const auto &missing_functions = processed_file.get_missing_functions();
-        if (missing_functions.empty()) {
-            report_message << "-> No unlisted functions found.\n\n";
-        }
-        else {
+        if (!missing_functions.empty()) {
             has_issues = true;
+            report_message << "-- 3) UNLISTED FUNCTIONS --\n\n";
             for (const auto &missing_function : missing_functions) {
                 for (const auto &function : missing_function.functions) {
                     report_message << missing_function.line.number << "| " << missing_function.line.text << '\n'
@@ -88,16 +78,14 @@ namespace {
         }
     }
 
+    // If no issues were found, add an OK message
+    if (!has_issues) {
+        report_message << "-> OK.\n\n";
+    }
+
     report_message << line_separator;
 
-    if (has_issues) {
-        // Return the report message as a string
-        return report_message.str();
-    }
-    else {
-        // Return only the header, plus a message indicating that no issues were found
-        return "##- " + file_path + " -##\n\n-> OK.\n\n" + line_separator;
-    }
+    return "##- " + file_path + " -##\n\n" + report_message.str();
 }
 
 }  // namespace
